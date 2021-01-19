68daa85f203a96213c555b675a3872718f466d0e
caledfwlch-aws <77695842+caledfwlch-aws@users.noreply.github.com>
2021-01-19 16:21:32 -0500

    Create osinfo.py

---
+++ osinfo.py
@@ -0,0 +1,13 @@
+import platform
+print('Uname:', platform.uname())
+
+print()
+#print('Distribution :', platform.linux_distribution())
+#print('Distribution :', distro.linux_distribution())
+print('Machine :', platform.machine())
+print('Node :', platform.node())
+print('Processor :', platform.processor())
+print('Release :', platform.release())
+print('System :', platform.system())
+print('Version :', platform.version())
+print(‘Platform :', platform.platform())
